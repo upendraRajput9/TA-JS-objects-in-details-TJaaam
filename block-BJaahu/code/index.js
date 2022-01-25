c5ac3862a44fd1fdbf73ff4efa1b59f9587b226a
upendra <upendrarajput9911@gmail.com>
2022-01-26 03:21:39 +0530

    done

--- block-BJaahu/code/index.js
+++ block-BJaahu/code/index.js
@@ -1,26 +1,63 @@
-question.prototype={
-    isAnswerCorrect:function(index){
+// Prototypal pattern
+// let methods={
+//     isAnswerCorrect:function(index){
+//         return index=== this.correctAnswerIndex
+//     },
+//     getCorrectAnswer:function(){
+//         return this.options[this.correctAnswerIndex]
+//     }
+// }
+
+// function Question(title,options,correctAnswerIndex){
+//     let obj = Object.create(methods)
+// obj.title=title;
+// obj.options=options;
+// obj.correctAnswerIndex=correctAnswerIndex;
+// return obj
+// }
+
+
+
+
+// Pseudoclassical Pattern
+// Question.prototype={
+//     isAnswerCorrect:function(index){
+//         return index=== this.correctAnswerIndex
+//     },
+//     getCorrectAnswer:function(){
+//         return this.options[this.correctAnswerIndex]
+//     }
+// }
+
+// function Question(title,options,correctAnswerIndex){
+// this.title=title;
+// this.options=options;
+// this.correctAnswerIndex=correctAnswerIndex;
+// }
+
+// class
+class Question{
+    constructor(title,options,correctAnswerIndex){
+        this.title=title;
+        this.options=options;
+        this.correctAnswerIndex=correctAnswerIndex;
+    }
+    isAnswerCorrect(index){
         return index=== this.correctAnswerIndex
-    },
-    getCorrectAnswer:function(){
+    }
+    getCorrectAnswer(){
         return this.options[this.correctAnswerIndex]
     }
 }
 
-function question(title,options,correctAnswerIndex){
-this.title=title;
-this.options=options;
-this.correctAnswerIndex=correctAnswerIndex;
-}
 
 
-
-let firstQuestion = new question(
+let firstQuestion = new Question(
     'Where is the capital of Jordan',
     ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'],
     1
   );
-  let secondQuestion = new question(
+  let secondQuestion = new Question(
     'Where is the capital of Jamaica',
     ['Tashkent', 'Amaan', 'Kingston', 'Nairobi'],
     2
